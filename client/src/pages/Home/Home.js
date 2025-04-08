bb0f8dff00eee1df383ec1b4090bd84e113313f7
Chukwuemeka, Samuel Chinaza <samchuks898@gmail.com>
2025-04-08 21:53:54 +0100

    Designed home page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,12 +1,36 @@
 import React from "react";
 import "./Home.css";
+import Typewriter from "typewriter-effect";
+import Resume from "../../assets/docs/Resume.pdf";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-      <p>Welcome to the home page!</p>
-    </div>
+    <>
+      <div className="container-fluid home-container">
+        <div className="container home-content">
+          <h1>Hi, I am</h1>
+          <h2>
+            <Typewriter
+              options={{
+                strings: [
+                  "Chukwuemeka, Samuel",
+                  "A Full Stack Web Developer",
+                  "A Software Engineer",
+                ],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </h2>
+          <div className="home-buttons">
+            <button className="btn btn-hire">Hire Me</button>
+            <a className="btn btn-cv" href={Resume} download="Chukwuemeka.pdf">
+              My Resume
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
