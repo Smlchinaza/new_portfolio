d56a5dfd97322197eb9f39c80ab9e4692838fb5e
Chukwuemeka, Samuel Chinaza <samchuks898@gmail.com>
2025-04-12 09:38:59 +0100

    Added vercel analytics

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "./Context/ThemeContext";
+import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ThemeProvider>
     <React.StrictMode>
       <App />
+      <Analytics />
     </React.StrictMode>
   </ThemeProvider>
 );
